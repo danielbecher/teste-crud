960ec0a067e9061b7803703c5a84e235955ec688
Daniel <daniel@becher.com.br>
2022-03-02 05:47:16 -0300

    Corrigidos erros de link de folha de estilos

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/reset.css">
-    <link rel="stylesheet" href="/assets/css/estilos.css">
+    <link rel="stylesheet" href="./assets/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/estilos.css">
     <title>Cadastro de Funcionários</title>
 </head>
 <body>
