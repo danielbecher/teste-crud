b91795d6279ebe550c7a7a08baaf39361967d07a
Daniel <daniel@becher.com.br>
2022-03-02 04:17:41 -0300

    Adicionando página de erro de CPF inválido

---
+++ cpfinvalido.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de funcionários | Erro</title>
+</head>
+<body>
+    <?php $previous = "javascript:history.go(-1)"; ?>
+    <p>CPF inválido! <a href="<?= $previous ?>">Retorne</a> e tente novamente</p>
+</body>
+</html>
