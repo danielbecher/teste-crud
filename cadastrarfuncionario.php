8b8608abc8e03759d594724ee20bb5f872be5702
Daniel <daniel@becher.com.br>
2022-03-02 05:18:24 -0300

    Atualizado script de cadastro de funcionários

--- cadastrarfuncionario.php
+++ cadastrarfuncionario.php
@@ -1,37 +1,28 @@
 <?php
-include_once "validarcpf.php";
 //Importa a biblioteca de conexão ao DB.
 include_once "conecta.php";
-
-try {
-    $cpf = validaCPF($_POST['cpf']);
-    // Verifica a resposta da função e exibe na tela
-    if($cpf == true){
-        echo "CPF VDDR!";
-    }
-    elseif ($cpf == false){
-        echo "CPF FALSO";
-    }
-} catch(PDOException $erro) {
-    echo "CPF FALSO!";
-}
+include_once "validarcpf.php";
 
 try {
     $nome = filter_var($_POST['nome']);
-    $nasc = filter_var($_POST['nasc']);
-    $cpf = filter_var($_POST['cpf']);
+    $datanasc = filter_var($_POST['nasc']);
+    $nasc = implode("-",array_reverse(explode("/",$datanasc)));
+    $cpf = filter_var($_POST['cpf']);   
     $email = filter_var($_POST['email']);
     $civil = filter_var($_POST['civil']);
 
-    $inserir = $conectabanco->prepare('INSERT INTO funcionarios (nome,nasc,cpf,email,civil) VALUES (:nome, :nasc, :cpf, :email, :civil)');
-    $inserir->bindParam(':nome', $nome);
-    $inserir->bindParam(':nasc', $nasc);
-    $inserir->bindParam(':cpf', $cpf);
-    $inserir->bindParam(':email', $email);
-    $inserir->bindParam(':civil', $civil);
-    $inserir->execute();
-
-    header("location: index.php");
+    if (validaCPF($cpf) == true) {
+        $inserir = $conectabanco->prepare('INSERT INTO funcionarios (nome,nasc,cpf,email,civil) VALUES (:nome, :nasc, :cpf, :email, :civil)');
+        $inserir->bindParam(':nome', $nome);
+        $inserir->bindParam(':nasc', $nasc);
+        $inserir->bindParam(':cpf', $cpf);
+        $inserir->bindParam(':email', $email);
+        $inserir->bindParam(':civil', $civil);
+        $inserir->execute();
+        header("location: index.php");
+    } else {
+        header("location: cpfinvalido.php");
+    }
 
 } catch(PDOException $erro) {
     echo 'Erro: ' . $erro-> getMessage();
