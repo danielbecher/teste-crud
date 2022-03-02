d472c58ff7500896a151520ca6230fb3aadb54ea
Daniel <daniel@becher.com.br>
2022-03-02 05:19:18 -0300

    Estilizado o form de edição de funcionários

--- formeditar.php
+++ formeditar.php
@@ -1,19 +1,32 @@
-<?php
-include_once 'conecta.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/reset.css">
+    <link rel="stylesheet" href="/assets/css/estilos.css">
+    <title>Editar funcionário</title>
+</head>
+<body>
+    <?php
+    include_once 'conecta.php';
 
-$id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
-$consulta = $conectabanco->query("SELECT * from funcionarios WHERE id = '$id'");
-$linha = $consulta->fetch(PDO::FETCH_ASSOC);
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+    $consulta = $conectabanco->query("SELECT * from funcionarios WHERE id = '$id'");
+    $linha = $consulta->fetch(PDO::FETCH_ASSOC);
 
-?>
-
-<form action="editafuncionario.php" method="POST">
-    <label>Nome: <input type="text" name="nome" id="nome" value="<?php echo $linha['nome']?>"><br>
-    <label>Data de Nascimento: <input type="text" name="nasc" id="nasc"value="<?php echo $linha['nasc']?>"><br>
-    <label>CPF: <input type="text" name="cpf" id="cpf" value="<?php echo $linha['cpf']?>"><br>
-    <label>E-mail: <input type="text" name="email" id="email" value="<?php echo $linha['email']?>"><br>
-    <label>Estado Civil: <input type="text" name="civil" id="civil" value="<?php echo $linha['civil']?>"><br>
-    <input type="hidden" name="id" value="<?php echo $linha['id'] ?>">
-    <input type="submit" value="Salvar">
-</form>
-
+    ?>
+    <div class="container">
+        <a href="index.php"><img class="home__banner" src='./assets/img/banner.png'></a>
+        <form class="formulario" action="editafuncionario.php" method="POST">
+            <p>Nome:</p><input class="formulario__input" type="text" name="nome" id="nome" value="<?php echo $linha['nome']?>"><br>
+            <p>Data de Nascimento:</p><input class="formulario__input" type="date" name="nasc" id="nasc"value="<?php echo $linha['nasc']?>"><br>
+            <p>CPF:</p><input class="formulario__input" type="text" name="cpf" id="cpf" value="<?php echo $linha['cpf']?>"><br>
+            <p>E-mail:</p><input class="formulario__input" type="email" name="email" id="email" value="<?php echo $linha['email']?>"><br>
+            <p>Estado Civil:</p><input class="formulario__input" type="text" name="civil" id="civil" value="<?php echo $linha['civil']?>"><br>
+            <input type="hidden" name="id" value="<?php echo $linha['id'] ?>">
+            <input class="formulario__input--botao" type="submit" value="Salvar">
+        </form>
+</body>
+</html>
